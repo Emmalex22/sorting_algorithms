58e5cf48c1abf37130fd2e565cba38896801d9ea
Emmalex22 <emmanuelpelumi22@gmail.com>
2024-05-04 17:23:15 -0700

    updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,9 +8,9 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *temp, *prev_temp, *next_current;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
 	current = (*list)->next;
 	while (current != NULL)
 	{
